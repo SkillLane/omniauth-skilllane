11be58ad42d4c9b0e30adf59425bea4d4462c004
manotien <manotien@skilllane.com>
2021-04-07 09:23:37 +0700

    reverted code

--- lib/omniauth/strategies/skilllane.rb
+++ lib/omniauth/strategies/skilllane.rb
@@ -13,9 +13,6 @@ module OmniAuth
       }
 
       def client_options
-        params = request.env["omniauth.params"]
-        acr_values = params["acr_values"]
-        options.client_options[:acr_values] = acr_values
         options.client_options.merge(options.client_options)
       end
 
